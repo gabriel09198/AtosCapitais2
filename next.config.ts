19a6ac567b0c5c97f21d31c59809ddc88556fad8
Amauriotjr <amauri.junior@sergipetec.org.br>
2025-04-24 12:04:32 -0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://atos-capital-backend-docker.onrender.com/api/:path*',
+      },
+    ]
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+module.exports = nextConfig
 
-export default nextConfig;
