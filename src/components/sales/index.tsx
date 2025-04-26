2f246e782aad51232262551bca48c48c941c5dde
Amauriotjr <amauri.junior@sergipetec.org.br>
2025-04-26 11:12:21 -0300

    Add files via upload

--- src/components/sales/index.tsx
+++ src/components/sales/index.tsx
@@ -1,12 +1,38 @@
-import { CircleDollarSign } from "lucide-react";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
-import {Avatar, AvatarImage } from "../ui/avatar";
-import { AvatarFallback } from "@radix-ui/react-avatar";
+'use client'
+
+import { useEffect, useState } from 'react'
+import { CircleDollarSign } from 'lucide-react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from '../ui/card'
+import { Avatar, AvatarImage } from '../ui/avatar'
+import { AvatarFallback } from '@radix-ui/react-avatar'
+import { fetchDailySalesData } from '@/services/dateService'
 
 export function Sales() {
-    return (
-        <Card className="flex-1">
-            <CardHeader>
+  const [branches, setBranches] = useState<{ name: string; cnpj: string }[]>([])
+
+  useEffect(() => {
+    const carregarBranches = async () => {
+      try {
+        const token = localStorage.getItem('token') || ''
+        const { branchs } = await fetchDailySalesData(token)
+        setBranches(branchs)
+      } catch (err) {
+        console.error('Erro ao buscar filiais:', err)
+      }
+    }
+
+    carregarBranches()
+  }, [])
+
+  return (
+    <Card className="flex-1">
+      <CardHeader>
         <div className="flex items-center justify-center">
           <CardTitle className="text-lg sm:text-xl text-gray-800">
             Histórico de Consulta
@@ -14,68 +40,22 @@ export function Sales() {
           <CircleDollarSign className="ml-auto w-4 h-4" />
         </div>
       </CardHeader>
-      <CardDescription className="">
-
-
-      </CardDescription>
+      <CardDescription className=""></CardDescription>
       <CardContent>
-        <article className="flex items-center gap-2 border-b py-2">
-           <Avatar className="w-10 h-10">
-            
-            <AvatarImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3Pbi7RMtolmaxfBaGDQzgJSzymVRtx9UKNA&s"/>
-            <AvatarFallback>DV</AvatarFallback>
-            </Avatar> 
+        {branches.map((branch, index) => (
+          <article key={index} className="flex items-center gap-2 border-b py-2">
+            <Avatar className="w-10 h-10">
+              <AvatarImage src={`https://ui-avatars.com/api/?name=${branch.name}`} />
+              <AvatarFallback>DV</AvatarFallback>
+            </Avatar>
 
             <div>
-                <p className="text-sm sm:text-base font-semibold">filial 4</p>
-                <span className="text-[12px] sm:text-sm text-gray-400">filial@gmail.com</span>
+              <p className="text-sm sm:text-base font-semibold">{branch.name}</p>
+              <span className="text-[12px] sm:text-sm text-gray-400">{branch.cnpj}</span>
             </div>
-
-        </article>
-
-        <article className="flex items-center gap-2 border-b py-2">
-           <Avatar className="w-10 h-10">
-            
-            <AvatarImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5cDghCymbHDaq9iKk5TZaBqi5Qdij_KHOhQ&s"/>
-            <AvatarFallback>DV</AvatarFallback>
-            </Avatar> 
-
-            <div>
-                <p className="text-sm sm:text-base font-semibold">filial 3</p>
-                <span className="text-[12px] sm:text-sm text-gray-400">banese@gmail.com</span>
-            </div>
-
-        </article>
-        <article className="flex items-center gap-2 border-b py-2">
-           <Avatar className="w-10 h-10">
-            
-            <AvatarImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXrwrKNnDa3lgd94rQ3zszeT7oUw_5tSkYtQ&s"/>
-            <AvatarFallback>DV</AvatarFallback>
-            </Avatar> 
-
-            <div>
-                <p className="text-sm sm:text-base font-semibold">Filial 2</p>
-                <span className="text-[12px] sm:text-sm text-gray-400">filial@gmail.com</span>
-            </div>
-
-        </article>
-
-        <article className="flex items-center gap-2 border-b py-2">
-           <Avatar className="w-10 h-10">
-            
-            <AvatarImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREl5bKzjsehGQToeHt_d2V0NtNtqKDI4qDag&s"/>
-            <AvatarFallback>DV</AvatarFallback>
-            </Avatar> 
-
-            <div>
-                <p className="text-sm sm:text-base font-semibold">Filial 1</p>
-                <span className="text-[12px] sm:text-sm text-gray-400">filial@gmail.com</span>
-            </div>
-
-        </article>
+          </article>
+        ))}
       </CardContent>
-      </Card>
-    )
+    </Card>
+  )
 }
-
-
